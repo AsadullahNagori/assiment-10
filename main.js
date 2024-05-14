383dcf2c5bb8d630e507aa2dcda208e78895d0f7
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 16:54:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//i perfone a addition
+console.log(45 + 5);
+//i perfome a massige my frand in variabbel
+let Myname = "Asadullah";
+let Myfreand = "Arslan come in ground";
+console.log(`${Myname}, ${Myfreand}`);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//i perfone a addition
+
+console.log(45+5)
+
+//i perfome a massige my frand in variabbel
+let Myname:string= "Asadullah"
+
+let Myfreand: string="Arslan come in ground"
+console.log(`${Myname}, ${Myfreand}`);
